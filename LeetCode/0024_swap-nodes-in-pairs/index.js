55ce9e7fb0e8593c40a7b108b542c7f91ece8d12
Yiran <fangyira@gmail.com>
2022-02-18 01:50:16 +0900

    update for 2.15 2.16

---
+++ LeetCode/0024_swap-nodes-in-pairs/index.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var swapPairs = function(head) {
+    let entry = head;
+    let before = null;
+    
+    if (entry && entry.next) {
+        head = head.next;
+    }
+    
+    while (entry && entry.next) {
+        const first = entry;
+        const second = entry.next;
+        const neibor = second.next;
+
+        first.next = neibor;
+        second.next = first;
+        entry = second;
+        if (before) {
+            before.next = entry;
+        }
+        before = entry.next;        
+        entry = before.next;
+    }
+    
+    return head;
+};
--- LeetCode/README.md
+++ LeetCode/README.md
@@ -3,6 +3,8 @@
 ## Finished
 
 - [x] 1. Two Sum (2022.2.9)
+- [x] 24. Swap Nodes in Pairs (2022.2.16)
+- [x] 39. Combination Sum (2022.2.17)
 - [x] 78. Subsets (2022.2.14)
 - [x] 96. Unique Binary Search Trees (2022.2.13)
 - [x] 100. Same Tree (2022.2.12)
