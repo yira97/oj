eaf2a6b590d94bd1f684dcd9a732bb0e8c5cc9f4
Yiran <fangyira@gmail.com>
2022-05-01 22:49:41 +0900

    daily update

--- LeetCode/0338_counting-bits/golang/main.go
+++ LeetCode/0338_counting-bits/golang/main.go
@@ -1,24 +1,26 @@
 package main
 
+import "fmt"
+
 func countBits(n int) []int {
-    res := make([]int, 0)
-    
-    for i := 0; i <= n; i++ {
-        // 奇数比偶数多一个1
-        if (i % 2 != 0) {
-            res = append(res, res[i-1] + 1)
-            continue
-        }
-        
-        s := fmt.Sprintf("%b", i)
-        count := 0
-        for _, c := range s {
-            if c == '1' {
-                count++
-            }
-        }
-        res = append(res, count)
-    }
-    
-    return res
+	res := make([]int, 0)
+
+	for i := 0; i <= n; i++ {
+		// 奇数比偶数多一个1
+		if i%2 != 0 {
+			res = append(res, res[i-1]+1)
+			continue
+		}
+
+		s := fmt.Sprintf("%b", i)
+		count := 0
+		for _, c := range s {
+			if c == '1' {
+				count++
+			}
+		}
+		res = append(res, count)
+	}
+
+	return res
 }
